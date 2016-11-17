71a8691c48a5856d29b7c7e71729d70707743bdc
Jacques Alberts <server.jac@gmail.com>
2016-11-17 16:06:43 +0200

    Cars headers.

--- app.js
+++ app.js
@@ -42,6 +42,13 @@ switch(env) {
   break;
 }
 
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // =======================
 // Routes ================
 // =======================
